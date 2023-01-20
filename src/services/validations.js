835591f9303dfff5747692993f195e13146ec124
Aya ELk <aya.elhakkouni@student.vinci.be>
2023-01-20 11:04:56 +0100

    deploy + test

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,5 +1,16 @@
+
 // TODO: Add here the function to validate
 
 const isEmpty = (label) => !label || label.length === 0;
+
+if (label === undefined) {
+    return false;
+  }
+  // eslint-disable-next-line no-useless-escape
+  const regex = /[^a-zA-Z0-9]/;
+  if (label.match(regex)) return true;
+
+exports.isEmpty = isEmpty;
+
 
 exports.isEmpty = isEmpty;
